6f478298d4a3504f5c2c2c0d2a83f0284c42b7e6
GabrielFilomeno <biel_floripa@hotmail.com>
2024-06-25 14:21:10 -0300

    Ex5

--- src/main/java/com/example/petshop/models/Pet.java
+++ src/main/java/com/example/petshop/models/Pet.java
@@ -1,4 +1,59 @@
 package com.example.petshop.models;
 
 public class Pet {
+
+    private String nomePet;
+    private String raca;
+    private double peso;
+    private double altura;
+    private String dataNascimento;
+    private String especie;
+
+    public String getNomePet() {
+        return nomePet;
+    }
+
+    public void setNomePet(String nomePet) {
+        this.nomePet = nomePet;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
 }
--- src/main/java/com/example/petshop/models/Tutor.java
+++ src/main/java/com/example/petshop/models/Tutor.java
@@ -1,4 +1,32 @@
 package com.example.petshop.models;
 
 public class Tutor {
+
+    private String nomePet;
+    private Integer idade;
+    private String endereco;
+
+    public String getNomePet() {
+        return nomePet;
+    }
+
+    public void setNomePet(String nomePet) {
+        this.nomePet = nomePet;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
